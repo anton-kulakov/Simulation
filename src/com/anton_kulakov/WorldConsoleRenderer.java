d6f2952c2f7e6d263a6e5c8374d12fb699d7bc99
anton kulakov <kulakov-akv@yandex.ru>
2023-12-05 21:05:23 +0300

    Change WorldConsoleRenderer class

--- src/com/anton_kulakov/WorldConsoleRenderer.java
+++ src/com/anton_kulakov/WorldConsoleRenderer.java
@@ -4,45 +4,34 @@ import com.anton_kulakov.entity.Entity;
 
 public class WorldConsoleRenderer {
     public void render(World world) {
+        System.out.println();
 
         for (int row = 9; row >= 0; row--) {
             String line = row + "|";
             for (int column = 0; column <= 9; column++) {
-                Coordinates coordinates = new Coordinates(row, column);
-                if (world.isCellEmpty(coordinates)) {
+                if (world.isCellEmpty(row, column)) {
                     line += "   ";
                 } else {
-                    line += getEntityImage(world.getEntity(coordinates));
+                    line += getEntityImage(world.getEntity(new Coordinates(row, column)));
                 }
             }
-
             System.out.println(line);
         }
+
+        System.out.println();
     }
 
-    private String setImageForEntity(Entity entity) {
-        switch (entity.getClass().getSimpleName()) {
-            case "House":
-                return "🏠";
-
-            case "Money":
-                return "\uD83D\uDCB0";
-
-            case "ProgrammingSchool":
-                return "\uD83C\uDF93";
-
-            case "Tree":
-                return "\uD83C\uDF33";
-
-            case "Employer":
-                return "\uD83D\uDC68\u200D\uD83D\uDCBC";
-
-            case "Junior":
-                return "\uD83D\uDC76";
-        }
-        return "";
-    }
     private String getEntityImage(Entity entity) {
-        return " " + setImageForEntity(entity) + " ";
+        String image = switch (entity.getClass().getSimpleName()) {
+            case "House" -> "🏠";
+            case "Money" -> "\uD83D\uDCB0";
+            case "ProgrammingSchool" -> "\uD83C\uDF93";
+            case "Tree" -> "\uD83C\uDF33";
+            case "Employer" -> "\uD83D\uDC68\u200D\uD83D\uDCBC";
+            case "Junior" -> "\uD83D\uDC76";
+            default -> "";
+        };
+
+        return " " + image + " ";
     }
 }
